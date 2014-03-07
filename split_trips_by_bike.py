077b830510deb48aa0c133b3d8db98125ee88c53
Nick Bennett <nick@goggl.es>
2014-03-06 23:14:04 -0600

    Add revenue column to trips_by_bike CSV.

--- split_trips_by_bike.py
+++ split_trips_by_bike.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
+import csv
 from datetime import datetime
 import json
-import csv
+from math import ceil
 import os
 
 PATH = './trips_by_bike/'
@@ -9,6 +10,36 @@ PATH = './trips_by_bike/'
 def date(date_string):
     return datetime.strptime(date_string, '%m/%d/%Y %H:%M')
 
+def number(int_string):
+    return int(int_string.replace(',', ''))
+
+def trip_cost(trip):
+    if trip['usertype']=='Subscriber':
+        cost = 0
+        if trip['tripduration'] > 30*60:
+            # 30-60 minutes: $1.50
+            cost += 1.5
+        if trip['tripduration'] > 60*60:
+            # 60-90 minutes: +$3.00
+            cost += 3.0
+        if trip['tripduration'] > 90*60:
+            # Each additional 30 minutes past 90 minutes: $6.00         
+            addl_duration = trip['tripduration'] - 90*60
+            cost += 6.0*ceil(addl_duration/(30.0*60))
+        return cost
+    else:
+        cost = 0
+        if trip['tripduration'] > 30*60:
+            # 30-60 minutes: $2.00
+            cost += 2.0
+        if trip['tripduration'] > 60*60:
+            # 60-90 minutes: $4.00
+            cost += 4.0
+        if trip['tripduration'] > 90*60:
+            # Each additional 30 minutes past 90 minutes: $8.00
+            addl_duration = trip['tripduration'] - 90*60
+            cost += 8.0*ceil(addl_duration/(30.0*60))
+        return cost
 
 if __name__=="__main__":
     trips_by_bikeid = {}
@@ -23,10 +54,12 @@ if __name__=="__main__":
                                     for station in stations['stationBeanList']}
 
     for bikeid, trips in trips_by_bikeid.iteritems():
-        print('Adding lat/long to data for bike {0}.'.format(bikeid))
+        print('Adding lat/long, trip overage cost to data for bike {0}.'.format(bikeid))
         for trip in trips:
             trip['from_lat'], trip['from_long'] = latlong_by_stationname[trip['from_station_name']]
             trip['to_lat'], trip['to_long'] = latlong_by_stationname[trip['to_station_name']]
+            trip['tripduration'] = number(trip['tripduration'])
+            trip['overage'] = trip_cost(trip)
         #
         print('Writing {0} trips for bike {1}.'.format(len(trips), bikeid))
         with open(os.path.join(PATH, bikeid + '.csv'), 'wb') as f:
